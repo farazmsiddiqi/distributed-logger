f35e892ce72774fc30976eae7ee34b11a7c3ff43
mkolla2 <mkolla2@sp23-cs425-3101.cs.illinois.edu>
2023-02-06 14:28:26 -0600

    space was already added, revert

--- node.go
+++ node.go
@@ -28,8 +28,8 @@ func (node Node) sendEvent(host string, port string, nodeName string) int {
 
 	conn, err := net.Dial("tcp", host+":"+port)
 
-	// send node name first
-	conn.Write([]byte(f.Sprintf("%s ", nodeName)))
+	// send node name first - already has space added in main 
+	conn.Write([]byte(f.Sprintf("%s", nodeName)))
 
 	if err != nil {
 		f.Fprintln(os.Stderr, "fatal err: %s", err.Error())
